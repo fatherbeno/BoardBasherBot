1603e8c42cdf2923efa28b14045a25e60060ee98
Benjamin Gulliver <50311839+fatherbeno@users.noreply.github.com>
2024-08-16 16:55:22 +1000

    Added new command
    - added /makefile
    - replies with a text file with contents matching what the user inputted

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,9 +1,11 @@
 import * as ping from "./ping"
 import * as reply from "./reply"
 import * as getmembers from "./return-members"
+import * as makefile from './makefile'
 
 export const commands = {
     ping,
     reply,
     getmembers,
+    makefile,
 };
---
+++ src/commands/makefile.ts
@@ -0,0 +1,29 @@
+import { ICommandInput } from "../interfaces/ICommandInput";
+import { SlashCommandBuilder} from "discord.js";
+import { validateTextChannel } from "./command-helper";
+import { promises } from "fs";
+
+export const data = new SlashCommandBuilder()
+  .setName("makefile")
+  .setDescription("Generates a text file")
+  .addStringOption((option) => {
+    return option
+        .setName("message")
+        .setDescription("This will be the message in the file")
+        .setRequired(true);
+});
+
+export const execute = async (commandInput: ICommandInput) => {
+  const interaction = commandInput.interaction;
+  const channel = await validateTextChannel(commandInput);
+  if (!channel) {
+    return;
+  }
+  
+  const filePath = "./src/generated-files/message.txt";
+  
+  // @ts-ignore
+  await promises.writeFile(filePath, interaction.options.getString("message"));
+  await channel.send({files: [filePath]});
+  return commandInput.interaction.reply("payload sent!");
+};
