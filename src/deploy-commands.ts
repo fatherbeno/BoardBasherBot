fdbd310e90a33bb6ca5360452cabda2e43b1abdb
Benjamin Gulliver <50311839+fatherbeno@users.noreply.github.com>
2024-09-02 11:48:20 +1000

    set default perms to 0
    - by setting the default perms to 0 each command needs to be overridden and have a role added so said role can use the command

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -6,7 +6,10 @@ import { ELoggerCategory } from "./typing-helpers/enums/ELoggerCategory";
 
 const logger = getLogger(ELoggerCategory.Core);
 
-const commandsData = Object.values(commands).map((command) => command.data);
+const commandsData = Object.values(commands).map((command) => {
+  command.data.setDefaultMemberPermissions(0);
+  return command.data;
+});
 
 const rest = new REST({ version: "10" }).setToken(config.BOT_TOKEN);
 
