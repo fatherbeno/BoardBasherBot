e8c7a765ce62a36bf66b6aa08503c3ef08e5e57d
Benjamin Gulliver <50311839+fatherbeno@users.noreply.github.com>
2025-03-31 23:57:50 +1100

    /setusertype update
    - Added functionality to display the roles that the member was being set to in the log embed.

--- src/commands/setusertype.ts
+++ src/commands/setusertype.ts
@@ -1,4 +1,4 @@
-import { EmbedBuilder, GuildMember, SlashCommandBuilder, TextChannel } from "discord.js";
+import { EmbedBuilder, GuildMember, Role, SlashCommandBuilder, TextChannel } from "discord.js";
 import { CCommandHelper } from "../helpers/CCommandHelper";
 import { GlobalProperties, UserTypeRoles } from "../helpers/.helpers";
 import { getColourBasedOnUserType, getUserTypeDisplayName } from "../helpers/OtherUtilitiesHelper";
@@ -61,7 +61,7 @@ export const setUserType = async (affectedMember: GuildMember, userMember: Guild
     if (roles.length === 0) throw new Error(`No roles have been added to the ${userType} user type, please add at least one role first.`);
 
     // logs the role change to a channel
-    await logChannel.send({embeds: [buildSetUserTypeEmbed(affectedMember, userMember, userType)]});
+    await logChannel.send({embeds: [buildSetUserTypeEmbed(affectedMember, userMember, userType, roles)]});
 
     // sets the member's roles based on the roles stored on the particular user type
     await affectedMember.roles.set(roles)
@@ -74,7 +74,7 @@ export const setUserType = async (affectedMember: GuildMember, userMember: Guild
  * @param userType Inputted user type when command was executed.
  * @author Benjamin Gulliver (fatherbeno)
  */
-const buildSetUserTypeEmbed = (affectedMember: GuildMember, userMember: GuildMember, userType: EUserType): EmbedBuilder => {
+const buildSetUserTypeEmbed = (affectedMember: GuildMember, userMember: GuildMember, userType: EUserType, roles: Role[]): EmbedBuilder => {
     // gets a display name for the found EUserType.
     const newUserTypeDisplay = getUserTypeDisplayName(userType);
 
@@ -89,6 +89,12 @@ const buildSetUserTypeEmbed = (affectedMember: GuildMember, userMember: GuildMem
     const foundAvatar = userMember.user.avatarURL();
     const userMemberAvatar = foundAvatar ? foundAvatar : undefined
 
+    // generate roles list
+    let rolesList: string = "";
+    roles.forEach((role) => {
+        rolesList = `${rolesList} ${role}`;
+    });
+
     // construct the embed
     return new EmbedBuilder()
         .setAuthor({ name: userMember.displayName, iconURL: userMemberAvatar })
@@ -101,5 +107,6 @@ const buildSetUserTypeEmbed = (affectedMember: GuildMember, userMember: GuildMem
             { name: "id", value: affectedMember.user.id, inline: true },
             { name: "old type", value: oldUserTypeDisplay, inline: true },
             { name: "new type", value: newUserTypeDisplay, inline: true },
+            { name: "set roles", value: rolesList, inline: true },
         );
 }
